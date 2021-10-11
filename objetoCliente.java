4d91da15a868232a1668c734514e2c8118c4e43e
Dejsilva <dejs_silva@hotmail.com>
2021-10-11 18:05:19 -0300

    projeto java

---
+++ objetoCliente.java
@@ -0,0 +1,43 @@
+package POO;
+
+import java.util.Scanner;
+
+public class objetoCliente {
+
+	public static void main(String[] args) {
+		
+		
+		cliente clien1 = new cliente();	
+		clien1.Nome();
+		clien1.Idade();
+		
+		
+	
+		if(clien1.getIdade()<18)
+		{	
+			System.out.println("\nInfelismente não podemos continuar pois você é menor de idade");
+		}
+		else
+		{
+		
+		clien1.renda();
+		clien1.TotalDespesas();
+		//clien1.rendaMeses();
+		clien1.valorSonho();
+		clien1.sobraMensal();
+		clien1.opcao1();
+		clien1.opcao2();
+		clien1.opcao3();
+		
+		if(clien1.getRendaMensal()>20000)
+	{
+			System.out.println("\n" + clien1.getNomeCliente()+"\nParabens você alcançou seu objetivo");
+	}
+	/*	else
+	{
+			System.out.println("\n"+clien1.getNomeCliente()+"\nInfelizmente Você mão alcançou seu objetivo");
+	}*/}
+		
+	
+	
+}}
